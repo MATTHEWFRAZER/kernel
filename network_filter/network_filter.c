f70c5b1e87bfb5b56fa1580e1340233d939162b6
Matthew Frazer <mfrazeriguess@gmail.com>
2021-01-09 21:13:48 -0800

    add bluetooth filter

--- network_filter/network_filter.c
+++ network_filter/network_filter.c
@@ -7,6 +7,7 @@
 #include <linux/tcp.h>
 #include <linux/udp.h>
 #include <linux/spinlock.h>
+#include <sys/socket.h>
 
 MODULE_DESCRIPTION("example of a network_filter");
 MODULE_LICENSE("GPL");
@@ -15,50 +16,101 @@ MODULE_AUTHOR("Matthew Frazer");
 // spinlock so we can see the actual ordering of filtering
 DEFINE_SPINLOCK(g_spinlock);
 
-static unsigned int network_filter_handler_ipv4_pre(void *priv,
+static unsigned int network_filter_handler_ipv4_in(void *priv,
 			       struct sk_buff *socket_buff,
 			       const struct nf_hook_state *state);
 
-static unsigned int network_filter_handler_ipv4_post(void *priv,
+static unsigned int network_filter_handler_ipv4_out(void *priv,
 			       struct sk_buff *socket_buff,
 			       const struct nf_hook_state *state);
 
-static unsigned int network_filter_handler_ipv6_pre(void *priv,
+static unsigned int network_filter_handler_ipv6_in(void *priv,
 			       struct sk_buff *socket_buff,
 			       const struct nf_hook_state *state);
 
-static unsigned int network_filter_handler_ipv6_post(void *priv,
+static unsigned int network_filter_handler_ipv6_out(void *priv,
 			       struct sk_buff *socket_buff,
 			       const struct nf_hook_state *state);
+
+static unsigned int network_filter_handler_bt_in(void *priv,
+			       struct sk_buff *socket_buff,
+			       const struct nf_hook_state *state);
+
+static unsigned int network_filter_handler_bt_out(void *priv,
+			       struct sk_buff *socket_buff,
+			       const struct nf_hook_state *state);
+
+static unsigned int network_filter_handler_local_in(void *priv,
+			       struct sk_buff *socket_buff,
+			       const struct nf_hook_state *state);
+
+static unsigned int network_filter_handler_local_out(void *priv,
+			       struct sk_buff *socket_buff,
+			       const struct nf_hook_state *state);
+
+
+// a good source https://programtip.com/en/art-101966
 
 // Handler registering struct 1
-static struct nf_hook_ops network_filter_hook_ipv4_pre_ops __read_mostly = {
-    .hook = network_filter_handler_ipv4_pre,
+static struct nf_hook_ops network_filter_hook_ipv4_in_ops __read_mostly = {
+    .hook = network_filter_handler_ipv4_in,
     .pf = NFPROTO_IPV4,
     .hooknum = NF_INET_PRE_ROUTING,
     .priority = NF_IP_PRI_FIRST // My hook will be run before any other netfilter hook
 };
 
 // Handler registering struct 2
-static struct nf_hook_ops network_filter_hook_ipv4_post_ops __read_mostly = {
-    .hook = network_filter_handler_ipv4_post,
+static struct nf_hook_ops network_filter_hook_ipv4_out_ops __read_mostly = {
+    .hook = network_filter_handler_ipv4_out,
     .pf = NFPROTO_IPV4,
     .hooknum = NF_INET_POST_ROUTING,
     .priority = NF_IP_PRI_FIRST // My hook will be run before any other netfilter hook
 };
 
 // Handler registering struct 3
-static struct nf_hook_ops network_filter_hook_ipv6_pre_ops __read_mostly = {
-    .hook = network_filter_handler_ipv6_pre,
+static struct nf_hook_ops network_filter_hook_ipv6_in_ops __read_mostly = {
+    .hook = network_filter_handler_ipv6_in,
     .pf = NFPROTO_IPV6,
     .hooknum = NF_INET_PRE_ROUTING,
     .priority = NF_IP_PRI_FIRST // My hook will be run before any other netfilter hook
 };
 
 // Handler registering struct 4
-static struct nf_hook_ops network_filter_hook_ipv6_post_ops __read_mostly = {
-    .hook = network_filter_handler_ipv6_post,
+static struct nf_hook_ops network_filter_hook_ipv6_out_ops __read_mostly = {
+    .hook = network_filter_handler_ipv6_out,
     .pf = NFPROTO_IPV6,
+    .hooknum = NF_INET_POST_ROUTING,
+    .priority = NF_IP_PRI_FIRST // My hook will be run before any other netfilter hook
+};
+
+// Handler registering struct 5
+static struct nf_hook_ops network_filter_hook_bt_in_ops __read_mostly = {
+    .hook = network_filter_handler_bt_in,
+    .pf = AF_BLUETOOTH,
+    .hooknum = NF_INET_PRE_ROUTING,
+    .priority = NF_IP_PRI_FIRST // My hook will be run before any other netfilter hook
+};
+
+// Handler registering struct 6
+static struct nf_hook_ops network_filter_hook_bt_out_ops __read_mostly = {
+    .hook = network_filter_handler_bt_out,
+    .pf = AF_BLUETOOTH,
+    .hooknum = NF_INET_POST_ROUTING,
+    .priority = NF_IP_PRI_FIRST // My hook will be run before any other netfilter hook
+};
+
+// Handler registering struct 7
+static struct nf_hook_ops network_filter_hook_local_in_ops __read_mostly = {
+    .hook = network_filter_handler_local_in,
+    .pf = AF_LOCAL,
+    .hooknum = NF_INET_PRE_ROUTING,
+    .priority = NF_IP_PRI_FIRST // My hook will be run before any other netfilter hook
+};
+
+// Handler registering struct 8
+static struct nf_hook_ops network_filter_hook_local_out_ops __read_mostly = {
+    .hook = network_filter_handler_local_out,
+    .pf = AF_LOCAL,
     .hooknum = NF_INET_POST_ROUTING,
     .priority = NF_IP_PRI_FIRST // My hook will be run before any other netfilter hook
 };
@@ -72,6 +124,7 @@ static unsigned int network_filter_handler_inner(void *priv,
 {
     struct iphdr *iph;
     struct udphdr *udph;
+		struct tcphdr *tcph;
 
     spin_lock(&g_spinlock);
 		printk(KERN_NOTICE "%s: message %s", __FUNCTION__, message);
@@ -83,9 +136,22 @@ static unsigned int network_filter_handler_inner(void *priv,
     }
 
     iph = ip_hdr(socket_buff);
+
+    if(iph == NULL)
+		{
+		    goto network_filter_exit;
+		}
+
+		printk(KERN_NOTICE "%s: protocol %d", __FUNCTION__, iph->protocol);
 	  if (iph->protocol == IPPROTO_UDP)
     {
 		    udph = udp_hdr(socket_buff);
+
+				if(udph == NULL)
+				{
+				    goto network_filter_exit;
+				}
+
 		    if (ntohs(udph->dest) == 53)
         {
              printk(KERN_NOTICE "%s: accept, upd, DNS", __FUNCTION__);
@@ -94,7 +160,13 @@ static unsigned int network_filter_handler_inner(void *priv,
 	  }
 	  else if (iph->protocol == IPPROTO_TCP)
     {
-        printk(KERN_NOTICE "%s: accept, tcp", __FUNCTION__);
+		    tcph = tcp_hdr(socket_buff);
+				if(tcph == NULL)
+				{
+				    goto network_filter_exit;
+				}
+
+        printk(KERN_NOTICE "%s: accept, tcp, dest %d", __FUNCTION__, ntohs(tcph->dest));
 		    goto network_filter_exit;
 	  }
 
@@ -103,50 +175,90 @@ network_filter_exit:
 	  return NF_ACCEPT;
 }
 
-static unsigned int network_filter_handler_ipv4_pre(void *priv,
+static unsigned int network_filter_handler_ipv4_in(void *priv,
 			       struct sk_buff *socket_buff,
 			       const struct nf_hook_state *state)
 {
      return network_filter_handler_inner(priv, socket_buff, state, "ipv4 pre");
 }
 
-static unsigned int network_filter_handler_ipv4_post(void *priv,
+static unsigned int network_filter_handler_ipv4_out(void *priv,
 			       struct sk_buff *socket_buff,
 			       const struct nf_hook_state *state)
 {
     return network_filter_handler_inner(priv, socket_buff, state, "ipv4 post");
 }
 
-static unsigned int network_filter_handler_ipv6_pre(void *priv,
+static unsigned int network_filter_handler_ipv6_in(void *priv,
 			       struct sk_buff *socket_buff,
 			       const struct nf_hook_state *state)
 {
      return network_filter_handler_inner(priv, socket_buff, state, "ipv6 pre");
 }
 
-static unsigned int network_filter_handler_ipv6_post(void *priv,
+static unsigned int network_filter_handler_ipv6_out(void *priv,
 			       struct sk_buff *socket_buff,
 			       const struct nf_hook_state *state)
 {
     return network_filter_handler_inner(priv, socket_buff, state, "ipv6 post");
 }
 
+static unsigned int network_filter_handler_bt_in(void *priv,
+			       struct sk_buff *socket_buff,
+			       const struct nf_hook_state *state)
+{
+	   printk(KERN_NOTICE "bluetooth in\n");
+     return network_filter_handler_inner(priv, socket_buff, state, "ipv6 pre");
+}
+
+static unsigned int network_filter_handler_bt_out(void *priv,
+			       struct sk_buff *socket_buff,
+			       const struct nf_hook_state *state)
+{
+	  printk(KERN_NOTICE "bluetooth out\n");
+    return network_filter_handler_inner(priv, socket_buff, state, "ipv6 post");
+}
+
+static unsigned int network_filter_handler_local_in(void *priv,
+			       struct sk_buff *socket_buff,
+			       const struct nf_hook_state *state)
+{
+	   printk(KERN_NOTICE "local in\n");
+     return network_filter_handler_inner(priv, socket_buff, state, "ipv6 pre");
+}
+
+static unsigned int network_filter_handler_local_out(void *priv,
+			       struct sk_buff *socket_buff,
+			       const struct nf_hook_state *state)
+{
+	  printk(KERN_NOTICE "local out\n");
+    return network_filter_handler_inner(priv, socket_buff, state, "ipv6 post");
+}
+
 
 static int __init network_filter_init(void)
 {
-	  nf_register_net_hook(&init_net, &network_filter_hook_ipv4_pre_ops);
-    nf_register_net_hook(&init_net, &network_filter_hook_ipv4_post_ops);
-    nf_register_net_hook(&init_net, &network_filter_hook_ipv6_pre_ops);
-    nf_register_net_hook(&init_net, &network_filter_hook_ipv6_post_ops);
+	  nf_register_net_hook(&init_net, &network_filter_hook_ipv4_in_ops);
+    nf_register_net_hook(&init_net, &network_filter_hook_ipv4_out_ops);
+    nf_register_net_hook(&init_net, &network_filter_hook_ipv6_in_ops);
+    nf_register_net_hook(&init_net, &network_filter_hook_ipv6_out_ops);
+		nf_register_net_hook(&init_net, &network_filter_hook_bt_in_ops);
+    nf_register_net_hook(&init_net, &network_filter_hook_bt_out_ops);
+		nf_register_net_hook(&init_net, &network_filter_hook_local_in_ops);
+    nf_register_net_hook(&init_net, &network_filter_hook_local_out_ops);
     return 0;
 }
 
 static void __exit network_filter_exit(void)
 {
-    nf_unregister_net_hook(&init_net, &network_filter_hook_ipv4_pre_ops);
-    nf_unregister_net_hook(&init_net, &network_filter_hook_ipv4_post_ops);
-    nf_unregister_net_hook(&init_net, &network_filter_hook_ipv6_pre_ops);
-    nf_unregister_net_hook(&init_net, &network_filter_hook_ipv6_post_ops);
+    nf_unregister_net_hook(&init_net, &network_filter_hook_ipv4_in_ops);
+    nf_unregister_net_hook(&init_net, &network_filter_hook_ipv4_out_ops);
+    nf_unregister_net_hook(&init_net, &network_filter_hook_ipv6_in_ops);
+    nf_unregister_net_hook(&init_net, &network_filter_hook_ipv6_out_ops);
+		nf_unregister_net_hook(&init_net, &network_filter_hook_bt_in_ops);
+    nf_unregister_net_hook(&init_net, &network_filter_hook_bt_out_ops);
+		nf_unregister_net_hook(&init_net, &network_filter_hook_local_in_ops);
+    nf_unregister_net_hook(&init_net, &network_filter_hook_local_out_ops);
 }
 
 module_init(network_filter_init);
